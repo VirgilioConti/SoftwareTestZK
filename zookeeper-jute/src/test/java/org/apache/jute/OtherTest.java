a6112ada9e7eb9982a868de8c03c41707472dab1
VirgilioConti <virgiliconti@gmail.com>
2023-04-08 21:06:09 +0200

    Repo without unnecessary tests

---
+++ zookeeper-jute/src/test/java/org/apache/jute/OtherTest.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author virgilio
+ *
+ */
+class OtherTest {
+
+	@Test
+	void test() {
+		System.out.print("VIRGILIO VIRGILIO VIRGILIO ");
+		assertTrue(true);
+	}
+
+}
